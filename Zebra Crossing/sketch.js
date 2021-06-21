0904d6703a5c9efc3308aad8f2406fedd89253ac
G7-WHJR <raveen.anand@whitehatjr.com>
2021-06-21 16:00:31 +0530

    First

---
+++ Zebra Crossing/sketch.js
@@ -0,0 +1,17 @@
+//Variable named "Stripes" to create a new group
+var stripes;
+
+function setup() {
+  createCanvas(450, 400);
+
+  //Assign stripes variable to a new Group()
+  stripes = new Group();
+
+  //Draw stripes using for loop
+}
+
+function draw() {
+  background("gray");
+
+  drawSprites();
+}
